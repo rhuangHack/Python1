d3cf09b57e380be73cb56e78d7a9b69f4f6c157f
albert huang <hrd000621@hotmail.com>
2015-12-25 16:39:00 -0800

    add the feature of time delay and repeat 3 times

--- takeBreak.py
+++ takeBreak.py
@@ -1,2 +1,11 @@
 import webbrowser
-webbrowser.open("http://www.baidu.com")
+import time
+#for x in range(0, 3):
+
+totle_breaks = 3
+break_count = 0
+print("This program startd ")
+while(break_count < totle_breaks):
+    time.sleep(10)
+    webbrowser.open("http://www.baidu.com")
+    break_count = break_count + 1
