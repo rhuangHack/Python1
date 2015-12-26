5e05aa3d8dbdde7bb9beb593fd337900d1f56db4
albert huang <hrd000621@hotmail.com>
2015-12-25 17:38:54 -0800

    add list files feature

---
+++ rename_files.py
@@ -0,0 +1,8 @@
+def rename_files():
+    #1) get file names from a folder
+    file_list = os.listdir(r"/home/renda/Documents/Programming Foundations with Python/python1/prank")
+    print(file_list + "\n")
+
+    #2) for each file, rename filename
+
+rename_files()
